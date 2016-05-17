b921afdef303ed5e584af98b7b520c1293e2f7eb
Martin Jacobs <Marty@d-i89-197-97.student.eduroam.uq.edu.au>
2016-05-17 15:37:18 +1000

    test

--- index.php
+++ index.php
@@ -64,7 +64,9 @@
                 $url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=%22london%20eye%22&key=AIzaSyDPQhe1MxS69bPLryapwhD6f-rF1TlJR5Q';
                 $searchResult = file_get_contents($url);
                 //parse output to get their place_id
-                echo $searchResult['place_id'];
+                // echo $searchResult['place_id'];
+                $json_output = json_decode($searchResult, true);
+                echo $json_output;
                 //after that, with the place_id make another request with that to get the rating.
 
                 ?>
