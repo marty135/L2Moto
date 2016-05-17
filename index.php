d22ed69d7b8e33153b0579ed557c5d6784651144
Martin Jacobs <Marty@d-i89-197-97.student.eduroam.uq.edu.au>
2016-05-17 14:13:51 +1000

    test

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                  if (($handle = fopen($file, "r")) !== FALSE) {
                    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                       $num = count($data);
-                        echo "<p> $num fields in line $row: <br /></p>\n";
+                    //    echo "<p> $num fields in line $row: <br /></p>\n";
                           $row++;
                           for ($c=0; $c < $num; $c++) {
                           //  echo $data[$c] . "<br />\n";
