c141adc6cc1c05cceb5c1681ed867ad96802315c
Martin Jacobs <Marty@d-i89-197-97.student.eduroam.uq.edu.au>
2016-05-17 11:45:45 +1000

    test

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 $("testButton").click(function(){
     $.ajax({url: "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=%22london%20eye%22&key=AIzaSyDPQhe1MxS69bPLryapwhD6f-rF1TlJR5Q", success: function(result){
-        $("#status").html(result);
+        alert(result);
     }});
 });
 
