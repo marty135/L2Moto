27f8f89437c8b972aaa1ee6282ebce65df7fe673
Martin Jacobs <Marty@d-i89-197-97.student.eduroam.uq.edu.au>
2016-05-17 13:14:59 +1000

    test

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                  $file = fopen($providers, 'r');
                 while (($line = fgetcsv($file)) !== FALSE) {
                   //$line is an array of the csv elements
-                echo "<script type='text/javascript'>alert('$line[0]');</script>";
+                  print_r($line);
                 }
                 fclose($file);
 
