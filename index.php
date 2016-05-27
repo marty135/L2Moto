55e119edbc39508d7d37f45bd7ba95b215077bf2
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-27 20:19:49 +1000

    test

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
               <button type="button" class="close" data-dismiss="modal">&times;</button>
               <h4 class="modal-title"> Web feature </h4>
             </div>
-            <div class="modal-body">
+          <!--  <div class="modal-body"> -->
 
               <div class="first-column">
                 <!-- Your first column here -->
@@ -104,7 +104,7 @@
           </div>
 
           </div>
-            </div>
+          <!--  </div> -->
             <div class="modal-footer">
             <!--  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
             </div>
