54e8b8c363e9bb7a659bded6a902ef5f6004f5dd
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-27 20:07:51 +1000

    test

--- index.php
+++ index.php
@@ -105,7 +105,9 @@
 
           </div>
             </div>
-
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
           </div>
 
         </div>
