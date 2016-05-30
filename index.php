31387efdfd93c825b52564492c6b2e46242e4c79
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-31 00:10:41 +1000

    test

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
               <div class="first-column">
                 <!-- Your first column here -->
                 <?php echo "Hello test"; ?>
-            <!--  <?php include("populate_feature_list.php"); ?> -->
+            
 
                 <div id="secondmap">
                 <!--Add second map, for seeing the clicked locations -->
