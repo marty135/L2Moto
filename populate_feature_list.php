94e1fe841bd8cbdbad818b92d5ed8d9de99aed4d
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-30 22:54:15 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -3,27 +3,21 @@
 //create a list and then store for each Q-Ride Provider, a list of mappings for their
 //properties to be added to the database.
 include 'database_info.php';
-// Create db connection
 
+// Create db connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 if (!$conn) {
  die('Could not connect: ' . mysql_error());
 }
 
 //query
-$stmt = $conn->prepare("SELECT * FROM qride ORDER BY rating DESC LIMIT 5");
-$stmt->execute();
+$query = mysql_query("SELECT * FROM qride ORDER BY rating DESC LIMIT 5");
 
-if(mysql_num_rows($stmt)){
-$select= '<select name="select">';
-while($rs=mysql_fetch_array($stmt)){
-      $select.='<option value="'.$rs['id'].'">'.$rs['name'].'</option>';
-  }
+while ($row = mysql_fetch_array($sql)){
+echo $row;
 }
-$select.='</select>';
-echo $select;
 
-$stmt->close();
-
+//close the db connection.
+mysqli_close($conn);
 
 ?>
