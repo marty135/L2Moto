686044a7c8d22c91bbece8cd5e8c4f637e9c6ede
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-30 23:22:01 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -12,14 +12,12 @@ if (!$conn) {
 //Query to find top 5 q-ride providers in Queensland.
 $result = mysql_query($conn,"SELECT name, rating FROM qride ORDER BY rating DESC LIMIT 5;");
 
-if ($result->num_rows > 0) {
+
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "Provider: " . $row["name"]. " - Rating: " . $row["rating"]. "<br>";
     }
-} else {
-    echo "0 results";
-}
+} 
 
 $conn->close();
 
