a09d8ba37ab346a7d2f874e2ca3e61d5a755435c
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-30 23:27:00 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "Provider: " . $row["name"]. " - Rating: " . $row["rating"]. "<br>";
     }
 } else {
     echo "0 results";
