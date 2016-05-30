71ae7bc702595dd71ae8103b37da95e557ae1d40
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-31 00:24:15 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -10,11 +10,19 @@ if (mysqli_connect_errno())
 
 // Perform queries
 if ($result = mysqli_query($con,"SELECT name, rating FROM qride ORDER BY rating DESC LIMIT 5")) {
-  printf("test");
-
 printf("Select returned %d rows.\n", $result->num_rows);
  /* free result set */
  $result->close();
 }
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Provider: " . $row["name"]. " - Rating: " . $row["rating"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
 mysqli_close($con);
 ?>
