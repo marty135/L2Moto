34ebccdb61155d5fb884e8e50f34fa20be81eaff
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-30 23:17:22 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -1,5 +1,4 @@
 <?php
-
 //create a list and then store for each Q-Ride Provider, a list of mappings for their
 //properties to be added to the database.
 include 'database_info.php';
@@ -11,13 +10,13 @@ if (!$conn) {
 }
 
 //Query to find top 5 q-ride providers in Queensland.
-$query = mysql_query("SELECT name, rating FROM qride ORDER BY rating DESC LIMIT 5");
+$query = mysql_query("SELECT name, rating FROM qride");
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Provider: " . $row["name"]. " - Rating: " . $row["rating"]. "<br>";
+        echo "Provider: " . $row[0]. " - Rating: " . $row[1]. "<br>";
     }
 } else {
     echo "0 results";
