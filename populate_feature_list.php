5ede4906322dfee93e89720b04d05e26344990d6
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-31 00:17:22 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -1,35 +1,18 @@
 <?php
 include 'database_info.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$con = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+// Perform queries
+if ($result = mysqli_query($con,"SELECT name, rating FROM table ORDER BY rating DESC LIMIT 5")) {
+printf("Select returned %d rows.\n", $result->num_rows);
+ /* free result set */
+ $result->close();
 }
-
-//Query to find top 5 q-ride providers in Queensland.
-$sql = "SELECT name, rating FROM table ORDER BY rating DESC LIMIT 5";
-
-$result = $conn->query($sql);
-
-/* Select queries return a resultset */
-if ($result = $mysqli->query($sql)) {
-    echo "Select returned %d rows.\n" . $result->num_rows);
-    /* free result set */
-    $result->close();
-}
-
-
-
-
-//if ($result->num_rows > 0) {
-    // output data of each row
-//    while($row = $result->fetch_assoc()) {
-//        echo "Provider: " . $row["name"]. " - Rating: " . $row["rating"]. "<br>";
-  //  }
-//} else {
-//    echo "0 results";
-//}
-mysqli_close($conn);
+mysqli_close($con);
 ?>
