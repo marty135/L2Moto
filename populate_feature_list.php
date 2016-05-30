99e827de8f1c68ee8c2dbda1f41d2b5a5750f1ad
Martin Jacobs <Marty@Martins-MacBook-Pro.local>
2016-05-31 00:01:08 +1000

    test

--- populate_feature_list.php
+++ populate_feature_list.php
@@ -13,6 +13,17 @@ $sql = "SELECT name, rating FROM table ORDER BY rating DESC LIMIT 5";
 
 $result = $conn->query($sql);
 
+/* Select queries return a resultset */
+if ($result = $mysqli->query($sql)) {
+    echo("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+
+
+
+
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
