5f054ce78cbd16d955ccd71144953cd16b5b0e22
Martin Jacobs <Marty@d-i89-196-121.student.eduroam.uq.edu.au>
2016-05-31 14:28:39 +1000

    test

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <link href="images/Instagram.png" type="image/css" rel="stylesheet" />
   <link href="images/LinkedIn.png" type="image/css" rel="stylesheet" />
 
-  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&v=3&libraries=geometry"></script>
+<!--  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&v=3&libraries=geometry"></script> -->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
