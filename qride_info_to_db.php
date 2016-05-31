e59611e6eb95ed980ddd55b8e7a98478897e6b3e
Martin Jacobs <Marty@d-i89-196-121.student.eduroam.uq.edu.au>
2016-05-31 14:36:29 +1000

    test

--- qride_info_to_db.php
+++ qride_info_to_db.php
@@ -106,7 +106,7 @@ $rating = $provider_ratings[$i-1];
     if (in_array($name, $scrape_Titles)) {
       $key = array_search($name, $scrape_Titles);
 
-      if ($rating < 1) {
+      if ($rating === NULL) {
         $rating = ($scrape_Ratings[$key]);
       } else {
         $rating = ($rating + $scrape_Ratings[$key])/2;
