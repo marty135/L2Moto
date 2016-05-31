ee0255ee1ddd447ab683aa662dd40744d664ae7b
Martin Jacobs <Marty@d-i89-196-121.student.eduroam.uq.edu.au>
2016-05-31 12:23:34 +1000

    test

--- webscrape.php
+++ webscrape.php
@@ -17,7 +17,7 @@ for ($i = 0; $i < count($scrape_URLS); $i++) {
 //  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
   curl_setopt( $ch, CURLOPT_HEADER, false );
   curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-  $data = curl_exec( $ch );
+  //$data = curl_exec( $ch );
 
   if (strpos($data, '_3-ma _2bne')) {
     $source1 = substr((string)$data, strpos((string)$data, '_3-ma _2bne') + 13, 3);
